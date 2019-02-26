ab8855985f532cd358603c7cc6c7e57948ad827a
3B Modular Info <3binfo@basilides.local>
2019-02-26 19:46:10 -0300

    Trab Java

---
+++ ProjetoLivraria/livraria03/src/br/com/bg/livraria03/bean/LivroBean.java
@@ -0,0 +1,12 @@
+package br.com.bg.livraria03.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+  
+
+	public void gravarLivro(){
+		System.out.println("Gravando Livro !");
+	}
+}
